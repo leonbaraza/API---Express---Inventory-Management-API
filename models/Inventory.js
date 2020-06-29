d5c7212cd736d5a5ff5b44ab3fd0b750ff01d0a6
Leon <leonbaraza@gmail.com>
2020-06-29 10:53:56 +0300

    Inventory Model

--- models/Inventory.js
+++ models/Inventory.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+const Stock = require('./Stock')
 
 const inventorySchema = new mongoose.Schema({
     name:{
@@ -22,6 +23,19 @@ const inventorySchema = new mongoose.Schema({
         default:Date.now,
         required:true
     }
+})
+
+inventorySchema.pre('remove', function(next) {
+    Stock.find({inventory:this.id}, (err, inventory) => {
+        if (err){
+            next(err)
+        }else if( inventory.length > 0 ){
+            next(new Error('This inventory has Stock details. Cannot be deleted'))
+        }
+        else{
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Inventory', inventorySchema)
